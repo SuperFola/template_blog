deadbd3840f75c101a2a6aa4f152ec9f80c0678c
Loodoor <urworld@outlook.fr>
2016-01-09 20:55:54 +0100

    config manager

---
+++ private/configmanager.php
@@ -0,0 +1,27 @@
+﻿<?php
+    class ConfigManager {
+        public function __construct() {
+            $this->path = realpath(__DIR__ . '/../private/config.json');
+        }
+        
+        /**
+         * Retourne la configuration
+         *
+         * @return array
+         */
+        public function getConfig() {
+            return json_decode(file_get_contents($this->path), true);
+        }
+        
+        /**
+         * Permet de configurer un champ spécifique de la config
+         *
+         * @param $key
+         */
+        public function setConfig($key) {
+            if (isset($this->getConfig()[$key])) {
+                // do some stuff
+            }
+        }
+    }
+?>
