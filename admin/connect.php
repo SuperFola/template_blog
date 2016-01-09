d96029e7eab35b39675fa635b69258854d318002
Loodoor <urworld@outlook.fr>
2016-01-09 17:29:15 +0100

    style connection

--- admin/connect.php
+++ admin/connect.php
@@ -1,6 +1,8 @@
-﻿            Merci de vous identifier : <br />
+﻿        <div class="connection">
+            Merci de vous identifier : <br />
             <form method="post" action="admin/index.php">
                 Utilisateur : <input type="text" name="user" /> <br />
                 Mot de passe : <input type="password" name="pwd" /> <br />
                 <button type="submit">Se connecter</button>
             </form>
+        </div>
