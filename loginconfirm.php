aafd0b60e573ddff17adb58328c147321716609d
Loodoor <urworld@outlook.fr>
2016-01-09 20:51:23 +0100

    Erreurs et login

---
+++ loginconfirm.php
@@ -0,0 +1,15 @@
+﻿<?php
+    if (isset($_POST['user']) and isset($_POST['pwd'])){
+        if (true) {
+            // connexion réussie
+            session_start();
+            $_SESSION[$_POST['user']] = array(
+                "pseudo" => $_POST['user'],
+                "pwd" => $_POST['pwd']
+            );
+            header("Location: index.php");
+        }
+    } else {
+        header("Location: error.php?error=connexion");
+    }
+?>
